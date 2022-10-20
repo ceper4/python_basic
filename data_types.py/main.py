06cc4fc31db30088a70a2360d667a06f3cd3ea00
User <serjiii90@gmail.com>
2022-10-20 21:39:25 +0100

    my first homework

---
+++ data_types.py/main.py
@@ -0,0 +1,22 @@
+first_name = 'Serhii'
+family_name = 'Herasymenko'
+nationalty = 'Ukraine'
+full_age = 32
+sex_ref = 'male'
+hobby = 'jogging'
+work_exp_years = 10
+favorit_sport = 'football'
+favorit_pet = 'cat'
+countries_visit = 7
+
+print(first_name, type(first_name))
+print(family_name, type(family_name))
+print(nationalty, type(nationalty))
+print(full_age, type(full_age))
+print(sex_ref, type(sex_ref))
+print(hobby, type(hobby))
+print(work_exp_years,type(work_exp_years))
+print(favorit_sport, type(favorit_sport))
+print(favorit_pet, type(favorit_pet))
+print(countries_visit, type(countries_visit))
+
