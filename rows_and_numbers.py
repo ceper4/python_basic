ec0db06a271105787f4cbbfec6211086cfdf8101
User <serjiii90@gmail.com>
2022-10-31 21:57:21 +0000

    my third homework part 2

---
+++ rows_and_numbers.py
@@ -0,0 +1,14 @@
+username = input("Please tell me your name: ")
+result = f"Hello {username}, nice to meet you!"
+print(result)
+
+numb = input("Please, enter a number: ")
+print(numb)
+integ_numb = int(float(numb))
+print(integ_numb)
+deg = integ_numb ** 4
+print(deg)
+root = integ_numb ** 0.5
+print(root)
+remaind = integ_numb % 2
+print(remaind)
