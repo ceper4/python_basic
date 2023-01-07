9aff5fa3947c4908e73fe59bd31fdb2e6ebc92d9
User <serjiii90@gmail.com>
2023-01-07 21:44:29 +0000

    homework virtualenv

---
+++ virtualenv_practice.py
@@ -0,0 +1,28 @@
+# Створив проект virtualenv.  Перезавантажив термінал, отримав помилку безпеки...
+# # Невозможно загрузить файл C:\Users\User\PycharmProjects\pythonProject\venv\virtualenv\Scripts\activate.ps1, так как
+# # выполнение сценариев отключено в этой системе. Для получения дополнительных сведений см. about_Execution_Policies
+# # по адресу https:/go.microsoft.com/fwlink/?LinkID=135170.
+# #     + CategoryInfo          : Ошибка безопасности: (:) [], ParentContainsErrorRecordException
+# #     + FullyQualifiedErrorId : UnauthorizedAccess
+# # PS C:\Users\User\PycharmProjects\python_basic>
+#
+# Перевірив чи встановленно щось за допомогою команди pip freeze
+# Нічого не було. Встановив за допомогою команди pip install , модулі requests , django, flask
+#
+# З командою pip freeze перевірив чи встановилися маодулі:
+# PS C:\Users\User\PycharmProjects\python_basic> pip freeze
+# asgiref==3.6.0
+# certifi==2022.12.7
+# charset-normalizer==2.1.1
+# click==8.1.3
+# colorama==0.4.6
+# Django==4.1.5
+# Flask==2.2.2
+# idna==3.4
+# itsdangerous==2.1.2
+# Jinja2==3.1.2
+# MarkupSafe==2.1.1
+# Створив requirements.txt
+# PS C:\Users\User\PycharmProjects\python_basic> pip > requirements.txt
+# PS C:\Users\User\PycharmProjects\python_basic>
+# Для того щоб деактувати віртуальне оточення просто закрив термінал.
