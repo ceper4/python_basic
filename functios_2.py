341c5b2ce01d94ab122e14f0135dda32553e01e8
User <serjiii90@gmail.com>
2023-01-07 21:43:59 +0000

    homework funcdecorator

---
+++ functios_2.py
@@ -0,0 +1,13 @@
+def decor(func_to_decor):
+    print("Tomato")
+    def warapper()
+        print("Cheese")
+        func_to_decor()
+    print("Meat")
+    return warapper
+
+@decor
+def main_func():
+    print("Bread")
+
+main_func()
